2d7ee2e821137423fcf9e4631b734f65e8eb70e6
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2025-05-16 15:59:15 +0300

    Added function that I can't implement as comment

--- src/parser.rs
+++ src/parser.rs
@@ -621,4 +621,42 @@ return 993322;
         }
         true
     }
+    //fn test_literal_expression(exp: &Box<dyn Expression>, expected)
+    //func testLiteralExpression(
+    //t *testing.T,
+    //exp ast.Expression,
+    //expected interface{},
+    //) bool {
+    //switch v := expected.(type) {
+    //case int:
+    //return testIntegerLiteral(t, exp, int64(v))
+    //case int64:
+    //return testIntegerLiteral(t, exp, v)
+    //case string:
+    //return testIdentifier(t, exp, v)
+    //}
+    //t.Errorf("type of exp not handled. got=%T", exp)
+    //return false
+    //}
+    //fn test_infix_expression(exp: &Box<dyn Expression>,left,operator: &str, right)
+    //func testInfixExpression(t *testing.T, exp ast.Expression, left interface{},
+    //operator string, right interface{}) bool {
+    //opExp, ok := exp.(*ast.InfixExpression)
+    //if !ok {
+    //t.Errorf("exp is not ast.OperatorExpression. got=%T(%s)", exp, exp)
+    //return false
+    //}
+    //if !testLiteralExpression(t, opExp.Left, left) {
+    //return false
+    //}
+    //if opExp.Operator != operator {
+    //t.Errorf("exp.Operator is not '%s'. got=%q", operator, opExp.Operator)
+    //return false
+    //}
+    //if !testLiteralExpression(t, opExp.Right, right) {
+    //return false
+    //}
+    //return true
+    //}
+    //
 }
