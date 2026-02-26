8903bcbbbc2bf1fef820740d14524849c34b636a
omar-abdelgawad <omar-abdelgawad@users.noreply.github.com>
2026-02-26 01:12:10 +0000

    deploy: 1174d27ab5c2e034260b372dde6f9d5ca4eaed51

--- monkey_rs.js
+++ monkey_rs.js
@@ -397,8 +397,8 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper204 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 72, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper180 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 67, __wbg_adapter_18);
         return ret;
     };
     imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
